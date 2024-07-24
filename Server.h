0c499b4bf50c25623adf1b71d4ef945b896914b0
Edu2metros <eduardosantos14.gomes@gmail.com>
2024-07-24 16:17:43 -0300

    integrar na branch atual

--- Server.h
+++ Server.h
@@ -15,25 +15,47 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <map>
 
 typedef struct sockaddr_in SockAddrIn;
 using namespace std;
 
+struct Location {
+    string path;
+    map<string, string> directives;
+};
+
 class Server {
-	public:
-		Server(void);
-    Server(char *);
-		Server(Server const &);
-		void run(void);
-		void sender(int, string);
-		std::string receiver(int, int);
-		Server & operator = (Server const &);
-		~Server(void);
-	private:
-		string	   host;
-    	string     port;
-		SockAddrIn address;
-		int        sock;
+    public:
+        Server(void);
+        Server(char *);
+        Server(Server const &);
+        void run(void);
+        void sender(int, string);
+        string receiver(int, int);
+        Server & operator = (Server const &);
+        ~Server(void);
+
+        void setPort(const string& port) { this->port = port; }
+        string getPort() const { return port; }
+
+        void setHost(const string& host) { this->host = host; }
+        string getHost() const { return host; }
+
+        void addLocation(const Location& location) { locations.push_back(location); }
+        
+        vector<Location> getLocations() const { return locations; }
+        vector<Location>::iterator getBegin() { return locations.begin(); }
+        vector<Location>::iterator getEnd() { return locations.end(); }
+        vector<Location>::const_iterator getBegin() const { return locations.begin(); }
+        vector<Location>::const_iterator getEnd() const { return locations.end(); }
+
+    private:
+        string host;
+        string port;
+        SockAddrIn address;
+        int sock;
+        vector<Location> locations;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,27 +1,5 @@
 #include "Server.h"
 #include "Stream.h"
-#include <map>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <stdexcept>
-
-using namespace std;
-
-struct Location
-{
-    string path;
-    map<string, string> directives;
-};
-
-class Server2
-{
-public:
-    string port;
-    string host;
-    vector<Location> locations;
-};
 
 static const std::string WHITESPACE = " \t\n\r\f\v";
 static const std::string COMMENT = "#";
@@ -56,18 +34,18 @@ void validateSemicolon(const std::string& line) {
     }
 }
 
-void processDirective(const std::string& line, Server2& config, Location& currentLocation, bool inLocation) {
+void processDirective(const std::string& line, Server& config, Location& currentLocation, bool inLocation) {
     std::string::size_type spacePos = line.find(" ");
     std::string directive = line.substr(0, spacePos);
     std::string value = line.substr(spacePos + 1);
 
     if (directive == "listen") {
-        config.port = value;
+        config.setPort(value);
     } else if (directive == "index" || directive == "root" || directive == "autoindex") {
         if (inLocation) {
             currentLocation.directives[directive] = value;
         } else {
-            config.host = value;
+            config.setHost(value);
         }
     } else {
         if (inLocation) {
@@ -79,74 +57,66 @@ void processDirective(const std::string& line, Server2& config, Location& curren
     }
 }
 
-Server2 parser(const char *file) {
+void erase(std::string &line) {
+    if(line.find(";") != std::string::npos)
+        line.erase(line.find(";"), 1);
+    if(line.find(OPEN_BRACKET) != std::string::npos)
+        line.erase(line.find(OPEN_BRACKET), 1);
+    if(line.find(CLOSE_BRACKET) != std::string::npos)
+        line.erase(line.find(CLOSE_BRACKET), 1);
+}
+
+Server parser(const char *file, int argc) {
+    if(argc == 1)
+        return Server();
     std::ifstream in(file);
     std::string line;
     Location currentLocation;
-    Server2 config;
+    Server config;
     bool inLocation = false;
     int bracketCount = 0;
 
     if (!in.is_open()) {
-        throw std::runtime_error("Could not open file");
+        cerr << "Error: Could not open file. Using default settings." << endl;
+        return Server();
     }
-
     while (std::getline(in, line)) {
-        if (skipEmptyLines(line, bracketCount)) {
+        if (skipEmptyLines(line, bracketCount))
             continue;
-        }
         if (line.find(CLOSE_BRACKET) != std::string::npos) {
             bracketCount--;
             if (inLocation) {
-                config.locations.push_back(currentLocation);
+                config.addLocation(currentLocation);
                 currentLocation = Location();
                 inLocation = false;
             }
             continue;
         }
         validateSemicolon(line);
+        erase(line);
         if (line.find("location") != std::string::npos) {
             inLocation = true;
             currentLocation.path = line.substr(line.find(" ") + 1);
             if (!currentLocation.path.empty() && currentLocation.path[currentLocation.path.size() - 1] == OPEN_BRACKET[0]) {
                 currentLocation.path.erase(currentLocation.path.size() - 1);
             }
-        } else {
+        } else
             processDirective(line, config, currentLocation, inLocation);
-        }
     }
     in.close();
     if (bracketCount != 0) {
-        std::cerr << "Error: Unmatched opening bracket." << std::endl;
-        throw std::runtime_error("Unmatched opening bracket");
+        std::cerr << "Error: Unmatched opening bracket. Using default settings." << std::endl;
+        return Server();
     }
-    return config;
+    if(config.getPort().empty())
+        config.setPort("8080");
+    if(config.getHost().empty())
+        config.setHost("localhost");
+    return(config);
 }
 
-
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        cerr << "Usage: " << argv[0] << " <config-file>" << endl;
-        return 1;
-    }
-
-    try {
-        Server2 server = parser(argv[1]);
-
-        cout << "Port: " << server.port << endl;
-        cout << "Host: " << server.host << endl;
-
-        for (vector<Location>::const_iterator it = server.locations.begin(); it != server.locations.end(); ++it) {
-            const Location& loc = *it;
-            cout << "Location: " << loc.path << endl;
-            for (map<string, string>::const_iterator dit = loc.directives.begin(); dit != loc.directives.end(); ++dit) {
-                cout << "  " << dit->first << ": " << dit->second << endl;
-            }
-        }
-    } catch (const exception& e) {
-        cerr << "Error parsing config file: " << e.what() << endl;
-        return 1;
-    }
-
+    Server server = parser(argv[1], argc);
+    server.run();
     return 0;
 }
