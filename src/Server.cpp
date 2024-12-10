c9d0cd8da1353b9e5df57d2a21c70692c78bbe76
Edu2metros <eduardosantos14.gomes@gmail.com>
2024-12-10 20:21:56 -0300

    90% terminado

--- src/Server.cpp
+++ src/Server.cpp
@@ -60,10 +60,6 @@ string Server::createPacket(int client) {
                             if (master.getFileName() != "") {
                                 out.open(path.c_str(), ios::out | ios::binary);
                             }
-                            if(piece == 192){
-                                cout << "ó entrou aqui ó\n" << buffer << endl;
-                                continue;
-                            }
                             if (!out.is_open())
                                 continue;
                             offset = (size_t)master.getHeaderLen();
